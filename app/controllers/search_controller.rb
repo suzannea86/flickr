0fbd64b1158db259861376882a4ade69c3dd820b
suzanne <suzanne.a86@gmail.com>
2014-08-06 12:07:47 +1000

    add pagination for search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
   def index
     if params[:search]
       flickr = FlickRaw::Flickr.new
-      @search_results = flickr.photos.search({:text => params[:search]})
+      @search_results = flickr.photos.search(:text => params[:search], :per_page => 10, :page => params[:page] || 1)
     end
   end
 
